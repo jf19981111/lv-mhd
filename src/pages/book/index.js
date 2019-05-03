bef34f87832796b8b00f90092dd41c03fa5cf7af
jf19981111 <983446103u@qq.com>
2019-05-03 13:31:07 +0800

    搜索与分页01

--- src/pages/book/index.js
+++ src/pages/book/index.js
@@ -2,65 +2,74 @@
 // 暴露出去的我们称之为样式组件
 import React from 'react'
 
-import { Input, Table } from 'antd'
+import { Input, Table, Button } from 'antd'
 import { BookWrap, MainWrap } from './style.js'
 
 // 引入 http 封装的 axios
 import http from '@/utils/http'
-
-const dataSource = [{
-    key: '1',
-    name: '李威',
-    age: 32,
-    address: '西湖区湖底公园1号',
-    value: 1
-  }, {
-    key: '2',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-    value: 2
-  }];
   
   const columns = [{
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-    filters: [{
-        text: '最帅的',
-        value: 1
-    }],
-    onFilter: (value, record) => {
-        console.log(value)
-        console.log(record)
-        return record.value === value
-    }
+      key: 'bookId',
+      title: 'bookId',
+      dataIndex: 'bookId'
+  },{
+      key: 'bookName',
+      title: '图书名',
+      dataIndex: 'bookName'
+  },{
+      key: 'author',
+      title: '作者',
+      dataIndex: 'author'
   }, {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-    sorter: function(a, b) {
-        return a.age - b.age
-    }
-  }, {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
+      key: 'coverurl',
+      title: '海报',
+      dataIndex: 'coverurl',
+      render: (text, record, index) => {
+        // console.log(text,record,index)
+        return <img src={text} alt="" />
+      }
   }];
 
 class Book extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            list: [],
+            pageNum: 1,
+            pageSize: 1,
+        }
+        // 搜索
+        this.getBookList = this.getBookList.bind(this)
+        // 分页器
+        this.onPageChange = this.onPageChange.bind(this)
+    }
+
     render() {
         return (
             <React.Fragment>
                 <BookWrap>
                     <div className="top">
                         <div className="left">
-                            <Input placeholder="请输入..."></Input>
+                            <Input 
+                                ref={(el) => this.InputEl = el} 
+                                placeholder="请输入...">
+                            </Input>
+                            <Button type="primary" onClick={this.getBookList}>
+                                搜索
+                            </Button>
                         </div>
                     </div>
 
                     <MainWrap>
-                        <Table dataSource={dataSource} columns={columns}></Table>
+                        <Table 
+                            dataSource={this.state.list} 
+                            columns={columns}
+                            rowKey="bookId"
+                            pagination={{ defaultPageSize: 1, total: 3, onChange: this.onPageChange}}
+                        >
+                        </Table>
                     </MainWrap>     
                 </BookWrap>   
             </React.Fragment>
@@ -71,10 +80,49 @@ class Book extends React.Component {
      * 发送请求
      */
     componentDidMount() {
-        http.get('/api/book')
-            .then(res => {
-                console.log(res)
-            })
+       this.getBookList()
+    }
+
+    /**
+     * 获取图书列表信息 getBookList 搜索
+     * 这里有一个小 bug 就是我们搜索的时候还要把 pageNumber 设置为1，
+     * 因为在别的页上搜索还是搜索的当前页
+     */
+    getBookList() {
+        // 这里是因为我们使用的 input 不是真实的的 DOM 元素，而是一个组件，所以使用 ref 拿不到对应的 value
+        // console.log(this.InputEl)
+        let { pageNum, pageSize } = this.state;
+        let bookName = this.InputEl.state.value;
+        http.get('/api/book', {
+            params: {
+                bookName,
+                pageNum,
+                pageSize,
+            }
+        })
+        .then(res => {
+            console.log(this.InputEl)
+            if (res.code === 0) {
+                this.setState(() => ({
+                    list: res.data.list,
+                }))
+                
+            }
+        })
+    }
+
+    /**
+     * 分页器的 onChange事件
+     */
+    onPageChange(page, pageSize) {
+        // console.log(page,pageSize)
+        this.setState(() => ({
+            pageNum: page,
+            pageSize: pageSize
+        }), () => {
+            this.getBookList()
+        })
+        
     }
 }
 
--- src/pages/book/style.js
+++ src/pages/book/style.js
@@ -2,14 +2,29 @@
 import styled from 'styled-components'
 
 export const BookWrap = styled.div`
+    background: antiquewhite;
     .top {
         display: flex;
         height: 40px;
         align-items: center;
         justify-content: space-between;
+
+        .left {
+            margin: 20px 0 0 20px;
+            display: flex;
+
+            button {
+                margin-left: 10px;
+            }
+        }
     }
 `
 
 export const MainWrap = styled.section`
-    
+    padding: 20px;
+    background: antiquewhite;
+    img {
+        width: 40px;
+        height: 50px;
+    }
 `
